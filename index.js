b5c850e5a0017a02435d4683d9c04c30b2df8c4d
Kanhaiya Sharma <kanaiya3112@gmail.com>
2020-11-15 18:20:27 +0530

    connected mongodb

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 let express = require('express');
+let mongoose = require('mongoose');
+var requestIp = require('request-ip');
+
 
 let app = express();
 
+let dbuser=process.env.dbuser || 'ktest';
+let dbpw = process.env.pass || '23ZtXLhiwyKv3CWe';
+
+mongoose.connect('mongodb+srv://'+dbuser+':'+dbpw+'@cluster0.fzeng.mongodb.net/sample_airbnb?retryWrites=true&w=majority',{useCreateIndex:true,useUnifiedTopology:true,useNewUrlParser:true}).then(()=>{
+    console.log('mongodb connected');
+}).catch((e)=>{  console.log(e); });
+
 app.get('/',(req,res)=>{
+    var clientIp = requestIp.getClientIp(req);
+    console.log(clientIp);
     console.log(process.env.dbuser);
     res.send('app starteddd');
 });
