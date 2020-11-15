c90dc0ac7188e841e3034d8c5435dea7edfba94e
Kanhaiya Sharma <kanaiya3112@gmail.com>
2020-11-15 17:23:52 +0530

    updated index

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ app.get('/',(req,res)=>{
     res.send('app starteddd');
 });
 
+port = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log('listening on port 3000');
+app.listen(port,()=>{
+    console.log('listening on port '+port);
 })
